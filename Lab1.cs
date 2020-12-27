376f9edb63c0141ef544cb3370203d8799eaac03
Hans2s <hansamui@outlook.com>
2020-12-28 01:14:27 +0300

    Created the variables

---
+++ Lab1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//writing the variables 
+
+		int x;
+		int y;
+		int a;
+		int b;
+		int c;
+		int d;
+
+		//added a b c d 
+		// for sin a = x
+		// for 2 sin a = 2 sin x = e
+		// for square root of c  = f
+		// for b x f = g
+		// e + cos g = h
+		// then h to the power of d  = y
+	}
+}
