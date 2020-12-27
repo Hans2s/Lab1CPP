6afff4c0f64fd43fa2a30ddf1bed65382bf40bc9
Hans2s <hansamui@outlook.com>
2020-12-28 01:32:19 +0300

    Changed the placements put it all under the Class1

--- Lab1.cs
+++ Lab1.cs
@@ -23,12 +23,21 @@ public class Class1
 		// *** bxf should be absolute ***
 		// 2.x + cos g = h
 		// then h to the power of d  = y
+
+		int x = math.sin(a);
+		int f = math.sqrt(c);
+		int g = Abs(b * f);
+		int h = 2 * x + math.cos(g);
+		int y = pow(h, d);
+
+
+
+		Console.WriteLine(y);
+
 	}
-	int x = math.sin(a);
-	int f = math.sqrt(c);
-	int g = Abs(b * f);
-	int h = 2 * x + math.cos(g);
-	int y = pow(h, d);
+
+
 	
 
 }
+
